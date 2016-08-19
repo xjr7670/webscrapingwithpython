adfcc63ab7b8bb8b2e99a45af7dc7e93e56d02c2
Cavin <xjr30226@126.com>
2016-08-19 20:59:47 +0800

    two send email program

---
+++ send_mail.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import smtplib
+from email.mime.text import MIMEText
+
+msg = MIMEText("The body of the email is here")
+
+msg['Subject'] = "An Email Alert"
+msg['From'] = 'xjr7670@sina.com'
+msg['To'] = 'xjr30226@126.com'
+
+s = smtplib.SMTP('smtp.sina.com')
+s.login('xjr7670@sina.com', 'x45668668')
+s.send_message(msg)
+s.quit()
---
+++ send_mail2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+import time
+import smtplib
+from email.mime.text import MIMEText
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def sendMail(subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = 'xjr7670@sina.com'
+    msg['To'] = 'xjr30226@126.com'
+
+    s = smtplib.SMTP()
+    s.connect('smtp.sina.com')
+    s.login('xjr7670@sina.com', 'x45668668')
+    s.send_message(msg)
+    s.quit()
+
+bsObj = BeautifulSoup(urlopen("https://isitchristmas.com/"), 'lxml')
+while (bsObj.find("a", {"id": "answer"}).noscript.get_text() == "不是"):
+    print("It is not Christmas yet.")
+    time.sleep(3600)
+sendMail("It's Christmas!", "According to http://itischristmas.com, it is Christmas!")
