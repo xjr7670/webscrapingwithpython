dac1658e8268ff8d87b96f6b0b8734bde01b1325
Cavin <xjr30226@126.com>
2016-08-23 21:36:51 +0800

    use in chapter 7 cleaning in code

---
+++ 2grams.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import re
+import string
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def cleanInput(content):
+    content = re.sub('\n+', " ", content)
+    content = re.sub('\[[0-9]*\]', "", content)
+    content = re.sub(' +', " ", content)
+    content = bytes(content, "UTF-8")
+    content = content.decode("ascii", "ignore")
+    cleanedInput = []
+    content = content.split(' ')
+    for item in content:
+        item = item.strip(string.punctuation)
+        if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
+            cleanedInput.append(item)
+    return cleanedInput
+
+
+def ngrams(content, n):
+    content = cleanInput(content)
+    output = []
+    for i in range(len(content)-n+1):
+        output.append(content[i:i+n])
+    return output
+
+html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
+bsObj = BeautifulSoup(html, 'lxml')
+content = bsObj.find("div", {"id": "mw-content-text"}).get_text()
+ngrams = ngrams(content, 2)
+print(ngrams)
+print("2-grams count is: " + str(len(ngrams)))
