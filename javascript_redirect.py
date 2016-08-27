4b6983e91500f92f1ec6729efd5eafc3c3d134e4
Cavin <xjr30226@126.com>
2016-08-27 21:13:21 +0800

    use in chapter10 - handling redirects

---
+++ javascript_redirect.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import time
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import StaleElementReferenceException
+
+def waitForLoad(driver):
+    elem = driver.find_element_by_tag_name("html")
+    count = 0
+    while True:
+        count += 1
+        if count > 20:
+            print("Timing out after 10 seconds and returning")
+            return
+        time.sleep(.5)
+        try:
+            elem == driver.find_element_by_tag_name("html")
+        except StaleElementReferenceException:
+            return
+
+driver = webdriver.PhantomJS(executable_path=r"D:\Python34\selenium\webdriver\phantomjs\bin\phantomjs.exe")
+driver.get("http://www.pythonscraping.com/pages/javascript/redirectDemo1.html")
+waitForLoad(driver)
+print(driver.page_source)
