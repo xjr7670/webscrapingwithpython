2d37d69a4982828b247ff25c3a5adda538d3bb70
Cavin <xjr30226@126.com>
2016-08-28 08:59:48 +0800

    use in chapter11 - Pillow

---
+++ use_pillow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+from PIL import ImageFilter
+
+kitten = Image.open("kitten.jpg")
+blurryKitten = kitten.filter(ImageFilter.GaussianBlur)
+blurryKitten.save("kitten_blurred.jpg")
+blurryKitten.show()
